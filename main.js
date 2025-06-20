0e73cf61a99db1f7b3be3713b0b7b03dff30f35b
MrShadow <camjekel@gmail.com>
2025-06-20 03:30:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import './map.js';
 import './charts.js';
+
+fetch('https://ipapi.co/json')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('info').innerHTML = \`
+      <strong>IP:</strong> \${data.ip} <br>
+      <strong>City:</strong> \${data.city} <br>
+      <strong>Region:</strong> \${data.region} <br>
+      <strong>Country:</strong> \${data.country_name} <br>
+      <strong>ISP:</strong> \${data.org}
+    \`;
+
+    window.renderMap(data.latitude, data.longitude);
+    window.updateChart(data.country_name);
+  });
